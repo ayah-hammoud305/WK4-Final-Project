8e5e068814607d5137cab1697fa68c63698b4f4e
ayah-hammoud305 <ayah.hammoud305@gmail.com>
2024-08-28 22:34:27 -0400

    fixed all

--- movie.js
+++ movie.js
@@ -1,2 +1,61 @@
-const movieListEl = document.querySelector('.movies')
-const year = localStorage.getItem("Year")
+const moviesContainer = document.querySelector(".movies");
+const searchBar = document.querySelector(".search__bar");
+const yearInput = document.querySelector('input[type="number"]');
+
+let movieResRaw = null;
+let movieResFilter = null;
+
+
+async function filterMovies(year) {
+
+  if (year) {
+    movieResFilter = await movieResRaw.filter((movie) => movie.Year === year);
+
+    moviesContainer.innerHTML = movieResFilter
+      .map((movie) => movieHTML(movie))
+      .join("");
+  } else {
+    moviesContainer.innerHTML = movieResRaw
+      .map((movie) => movieHTML(movie))
+      .join("");
+  }
+}
+
+async function loadMovies(searchTerm) {
+
+  try {
+    const response = await fetch(
+      `https://www.omdbapi.com/?apikey=ed88fe15&s=${searchTerm}`
+    );
+    const moviesData = await response.json();
+
+    if (moviesData.Search) {
+      movieResRaw = moviesData.Search;
+
+      moviesContainer.innerHTML = moviesData.Search.map((movie) =>
+        movieHTML(movie)
+      ).join("");
+    } else {
+      moviesContainer.innerHTML = `<p class="no__movies">No movies found.</p>`;
+    }
+  } catch (error) {
+    console.log("Error fetching movies: ", error);
+  }
+}
+
+
+function movieHTML(movie) {
+  return `<div class="movie">
+            <figure class="movie__img--wrapper">
+              <img src="${movie.Poster}" alt="${movie.Title}" class="movie__img">
+            </figure>
+            <div class="movie__desc">
+              <div class="movie__title">
+                ${movie.Title}
+              </div>
+              <div class="movie__year">
+                ${movie.Year}
+              </div>
+            </div>
+          </div>`;
+}
